eaa5b11c96b76bf3d7eebe04c2a9450a07ebb31e
Ivan Gomes <ivanjunior.pt@gmail.com>
2022-06-03 07:09:43 -0300

    finalizing header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Icon, Input, Text } from '@chakra-ui/react'
-import { RiSearchLine } from 'react-icons/ri'
+import { Flex, Icon, Input, HStack, Text, Box, Avatar } from '@chakra-ui/react'
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri'
 
 export function Header() {
     return (
@@ -13,7 +13,7 @@ export function Header() {
             px="6"
             align="center"
         >
-            <Text // o text é sempre utilizado para qualquer tipo de texto
+            <Text // o componente Text é sempre utilizado para qualquer tipo de texto
                 fontSize="3xl"
                 fontWeight="bold"
                 letterSpacing="tight"
@@ -46,6 +46,37 @@ export function Header() {
 
                 <Icon as={RiSearchLine} fontSize="20" />
             </Flex>
+
+            <Flex
+                align="center"
+                ml="auto"
+            >
+                <HStack
+                    mx="8"
+                    spacing="4"
+                    pr="8"
+                    py="1"
+                    color="gray.300"
+                    borderRightWidth={1}
+                    borderColor="gray.700"
+                >
+                    <Icon as={RiNotificationLine} fontSize="20" />
+                    <Icon as={RiUserAddLine} fontSize="20" />
+                </HStack>
+
+                <Flex align="center" textAlign="right">
+                    <Box mr="4" textAlign="right">
+                        <Text>Klein Moretti</Text>
+                        <Text color="gray.300" fontSize="small"
+                        >
+                            tarotclub@gmail.com
+                        </Text>
+                    </Box>
+
+                    <Avatar size="md" name="Klein Moretti" />
+                </Flex>
+            </Flex>
+
         </Flex>
     )
 }
