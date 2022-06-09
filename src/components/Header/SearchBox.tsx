8cbede270edba14aea6896b6df3bc54703d2a22d
Ivan Gomes <ivanjunior.pt@gmail.com>
2022-06-09 07:59:01 -0300

    finalizing forms

--- src/components/Header/SearchBox.tsx
+++ src/components/Header/SearchBox.tsx
@@ -1,34 +1,30 @@
 import { Flex, Icon, Input } from "@chakra-ui/react";
-import { useRef } from "react";
 import { RiSearchLine } from "react-icons/ri";
 
 export function SearchBox() {
-    const searchInputRef = useRef<HTMLInputElement>(null)
+  return (
+    <Flex
+      as="label"
+      flex="1"
+      py="4"
+      px="8"
+      maxWidth={400}
+      alignSelf="center"
+      color="gray.200"
+      position="relative"
+      bg="gray.800"
+      borderRadius="full"
+    >
+      <Input
+        color="gray.50"
+        variant="unstyled"
+        placeholder="Buscar na plataforma"
+        px="4"
+        mr="4"
+        _placeholder={{ color: "gray.400" }} // aplica cor apenas no texto do placeholder
+      />
 
-    return (
-        <Flex
-            as="label"
-            flex="1"
-            py="4"
-            px="8"
-            maxWidth={400}
-            alignSelf="center"
-            color="gray.200"
-            position="relative"
-            bg="gray.800"
-            borderRadius="full"
-        >
-            <Input
-                color="gray.50"
-                variant="unstyled"
-                placeholder="Buscar na plataforma"
-                px="4"
-                mr="4"
-                _placeholder={{ color: 'gray.400' }} // aplica cor apenas no texto do placeholder
-                ref={searchInputRef}
-            />
-
-            <Icon as={RiSearchLine} fontSize="20" />
-        </Flex>
-    )
+      <Icon as={RiSearchLine} fontSize="20" />
+    </Flex>
+  );
 }
