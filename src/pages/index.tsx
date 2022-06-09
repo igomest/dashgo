81fa12b3b352fadc0bb335d40e9210cce0c2bb1b
Ivan Gomes <ivanjunior.pt@gmail.com>
2022-06-09 07:33:19 -0300

    validating signin data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import { Button, Flex, Stack } from "@chakra-ui/react";
 import { SubmitHandler, useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 import { Input } from "../components/Form/Input";
 
 type SignInFormData = {
@@ -7,10 +9,17 @@ type SignInFormData = {
   password: string;
 };
 
-export default function SignIn() {
-  const { register, handleSubmit, formState } = useForm();
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
+  password: yup.string().required('Senha obrigatória'),
+});
 
-  const { isSubmitting } = formState;
+export default function SignIn() {
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(signInFormSchema),
+  });
+
+  const { isSubmitting, errors } = formState;
 
   const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
     await new Promise((resolve) => setTimeout(resolve, 2000));
@@ -36,6 +45,7 @@ export default function SignIn() {
             type="email"
             label="Email"
             {...register("email")}
+            error={errors.email}
           />
 
           <Input
@@ -43,6 +53,7 @@ export default function SignIn() {
             type="password"
             label="Senha"
             {...register("password")}
+            error={errors.password}
           />
         </Stack>
 
