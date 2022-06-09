c4ff025f3c4dcb163c49dbc4baeded5825a600a3
Ivan Gomes <ivanjunior.pt@gmail.com>
2022-06-09 10:23:25 -0300

    configuring axios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -22,6 +22,7 @@ import { Pagination } from "../../components/Pagination";
 import Link from "next/link";
 import { useEffect } from "react";
 import { useQuery } from "react-query";
+import { api } from "../../services/api";
 
 export default function UserList() {
   // data = os dados que minha requisição vai retornar,
@@ -30,8 +31,7 @@ export default function UserList() {
   const { data, isLoading, isFetching, error } = useQuery(
     "users",
     async () => {
-      const response = await fetch("http://localhost:3002/api/users");
-      const data = await response.json();
+      const { data } = await api.get("users");
 
       const users = data.users.map((user) => {
         return {
