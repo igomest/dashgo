d53afda536d5fe9e0014a845f8d058c1a54a7d59
Ivan Gomes <ivanjunior.pt@gmail.com>
2022-11-17 16:19:50 -0300

    Implementing test of error

--- src/tests/pages/SignIn.spec.tsx
+++ src/tests/pages/SignIn.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor, act, within } from "@testing-library/react";
 
 import { AuthProvider } from "contexts/AuthContext";
 
@@ -6,14 +6,18 @@ import { useRouter } from "next/router";
 
 import SignIn from "../../pages/index";
 
+import { api } from "services/apiClient";
+import MockAdapter from "axios-mock-adapter";
 
-jest.mock('next/router', () => ({
+jest.mock("next/router", () => ({
   useRouter: jest.fn().mockReturnValue({
-    push: jest.fn()
-  })
-}))
+    push: jest.fn(),
+  }),
+}));
 
-jest.mock('broadcast-channel')
+jest.mock("broadcast-channel");
+
+const mock = new MockAdapter(api);
 
 describe("SignIn page", () => {
   beforeEach(() => {
@@ -51,13 +55,57 @@ describe("SignIn page", () => {
     await waitFor(() => expect(screen.getByText("E-mail inválido")));
   });
 
-  it('should redirect the user to the dashboard page when signing In', async () => {
-    const useRouterMocked = jest.mocked(useRouter, { shallow: false })
-    const pushMock = jest.fn()
+  it("should redirect the user to the dashboard page when signing In", async () => {
+    const useRouterMocked = jest.mocked(useRouter, { shallow: false });
+    const pushMock = jest.fn();
+
+    mock.onPost("/sessions").reply(200, {
+      token: "fake-token",
+      refresh_token: "fake-refresh-token",
+      user: {
+        id: "fake-id",
+        name: "Joseph",
+        isAdmin: true,
+      },
+    });
 
     useRouterMocked.mockReturnValueOnce({
-      push: pushMock
-    } as any)
+      push: pushMock,
+    } as any);
+
+    fireEvent.input(screen.getByRole("textbox"), {
+      target: {
+        value: "test@email.com",
+      },
+    });
+
+    fireEvent.input(screen.getByLabelText("Senha"), {
+      target: {
+        value: "password",
+      },
+    });
+
+    const logIn = screen.getByRole("button", {
+      name: "Entrar",
+    });
+
+    fireEvent.click(logIn);
+
+    expect(screen.getByRole<HTMLInputElement>("textbox").value).toBe(
+      "test@email.com"
+    );
+
+    expect(screen.getByLabelText<HTMLInputElement>("Senha").value).toBe(
+      "password"
+    );
+
+    await waitFor(() => expect(useRouterMocked().push).toHaveBeenCalled());
+  });
+
+  it('should render a toast when an error occurs', async () => {
+    mock.onPost('/sessions').reply(400, {
+      message: 'Ops! Ocorreu um erro, tente novamente mais tarde.'
+    })
 
     fireEvent.input(screen.getByRole('textbox'), {
       target: {
@@ -75,9 +123,10 @@ describe("SignIn page", () => {
       name: 'Entrar'
     })
 
-    fireEvent.click(logIn)
+    act(() => {
+      fireEvent.click(logIn)
+    })
 
-    
     expect(screen.getByRole<HTMLInputElement>('textbox').value).toBe(
       'test@email.com'
     )
@@ -86,6 +135,8 @@ describe("SignIn page", () => {
       'password'
     )
 
-    await waitFor(() => expect(useRouterMocked().push).toHaveBeenCalled())
+    const listitem = screen.getByRole('listitem')
+
+    await waitFor(() => expect(within(listitem).getByRole('alert')))
   })
 });
